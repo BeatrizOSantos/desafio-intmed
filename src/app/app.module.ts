e9bce96a416ad5d49662892ed1b673a90c0aac7b
BeatrizOSantos <beatrizodsk@gmail.com>
2022-02-23 00:13:57 -0300

    feat: Main project structure

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,32 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { LoginComponent } from './features/login/login.component';
+import { HomeComponent } from './features/home/home.component';
+import { RegisterComponent } from './features/register/register.component';
+import { InputComponent } from './shared/input/input.component';
+import { ButtonComponent } from './shared/button/button.component';
+import { CircleProgressBarComponent } from './shared/circle-progress-bar/circle-progress-bar.component';
+import { ModalComponent } from './shared/modal/modal.component';
+import { SelectComponent } from './shared/select/select.component';
+import { HeaderComponent } from './core/header/header.component';
+import { ContainerComponent } from './core/container/container.component';
+import { RegisterAppointmentComponent } from './features/register-appointment/register-appointment.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    HomeComponent,
+    RegisterComponent,
+    InputComponent,
+    ButtonComponent,
+    CircleProgressBarComponent,
+    ModalComponent,
+    SelectComponent,
+    HeaderComponent,
+    ContainerComponent,
+    RegisterAppointmentComponent,
   ],
   imports: [
     BrowserModule,
